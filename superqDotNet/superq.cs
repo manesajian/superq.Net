786555ada872d2601dda49962222240d19395a9e
Daniel Manesajian <manesajian@gmail.com>
2015-02-05 21:28:55 -0800

    Parameterized superq class constructor.

--- superqDotNet/superq.cs
+++ superqDotNet/superq.cs
@@ -8,5 +8,16 @@ namespace superqDotNet
 {
     public class superq
     {
+        public superq(object initObj,
+                      string name,
+                      string host,
+                      bool attach,
+                      string keyCol,
+                      int maxlen,
+                      bool buildFromStr,
+                      bool buildFromFile)
+        {
+
+        }
     }
 }

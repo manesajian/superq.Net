39d331dfa851455833fee38898e4d0dc8b75ba24
Daniel Manesajian <manesajian@gmail.com>
2015-02-12 22:02:35 -0800

    Adding SuperQNetworkClientMgr class.

---
+++ superqDotNet/SuperQNetworkClientMgr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superqDotNet
+{
+    public class SuperQNetworkClientMgr
+    {
+        public SuperQNetworkClientMgr()
+        {
+
+        }
+    }
+}

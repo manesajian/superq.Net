932f184897224cf7494e0298ee053da929ecf1c7
Daniel Manesajian <manesajian@gmail.com>
2015-02-13 23:29:13 -0800

    Stubbed in class api.

--- superqDotNet/SuperQNodeResponse.cs
+++ superqDotNet/SuperQNodeResponse.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace superqDotNet
 {
     public class SuperQNodeResponse
     {
+        public string msg_id { get; set; }
+        public string result { get; set; }
+        public string body { get; set; }
+
         public SuperQNodeResponse()
         {
 

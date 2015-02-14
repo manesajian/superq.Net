684c6f2d50b060fbce82330cce23ca9700aeac21
Daniel Manesajian <manesajian@gmail.com>
2015-02-13 23:29:49 -0800

    Stubbed in class api.

--- superqDotNet/SuperQNodeRequest.cs
+++ superqDotNet/SuperQNodeRequest.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace superqDotNet
 {
     public class SuperQNodeRequest
     {
+        public string msg_id { get; set; }
+        public string cmd { get; set; }
+        public string args { get; set; }
+        public string body { get; set; }
+
+        private static int last_msg_id = 0;
+
         public SuperQNodeRequest()
         {
+            msg_id = get_msg_id().ToString();
+        }
 
+        public int get_msg_id()
+        {
+            return Interlocked.Increment(ref last_msg_id);
         }
     }
 }

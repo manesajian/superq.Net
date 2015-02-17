26f2817addaa7c3ca89e97ff7392518eff79ede2
Daniel Manesajian <manesajian@gmail.com>
2015-02-17 13:12:08 -0800

    Added from_str().

--- superqDotNet/SuperQNodeResponse.cs
+++ superqDotNet/SuperQNodeResponse.cs
@@ -17,5 +17,30 @@ namespace superqDotNet
         {
 
         }
+
+        public void from_str(string responseStr)
+        {
+            try
+            {
+                int headerSeparatorIdx = responseStr.IndexOf(";;");
+            
+                // separate out cmd header and body
+                string responseHeader = responseStr.Substring(0, headerSeparatorIdx);
+                string responseBody = responseStr.Substring(headerSeparatorIdx + 2);
+
+                string[] elems = responseHeader.Split('|');
+
+                if (elems.Count() != 2)
+                    throw new Exception("Malformed network response (" + responseStr + ")");
+
+                msg_id = elems[0];
+                result = elems[1];
+                body = responseBody;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Malformed network response (" + responseStr + "). Exception: " + e.Message);
+            }
+        }
     }
 }

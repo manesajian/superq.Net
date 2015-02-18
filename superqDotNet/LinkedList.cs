e867709a21f8b81dd040e594fa78c6b8270a72ae
Daniel Manesajian <manesajian@gmail.com>
2015-02-17 19:50:44 -0800

    Stubbed in api functions.

--- superqDotNet/LinkedList.cs
+++ superqDotNet/LinkedList.cs
@@ -8,5 +8,80 @@ namespace superqDotNet
 {
     public class LinkedList
     {
+        public LinkedListNode head = null;
+        public LinkedListNode tail = null;
+        public int count = 0;
+
+        public bool circular = false;
+
+        public LinkedList()
+        {
+
+        }
+
+        public bool is_empty()
+        {
+            return count == 0;
+        }
+
+        public void push(int idx, LinkedListNode node)
+        {
+
+        }
+
+        public void push_head(LinkedListNode node)
+        {
+
+        }
+
+        public void push_middle(int idx, LinkedListNode node)
+        {
+
+        }
+
+        public void push_tail(LinkedListNode node)
+        {
+
+        }
+
+        public LinkedListNode pop(int idx)
+        {
+            return null;
+        }
+
+        public LinkedListNode pop_head()
+        {
+            return null;
+        }
+
+        public LinkedListNode pop_tail()
+        {
+            return null;
+        }
+
+        public LinkedListNode pop_node(LinkedListNode node)
+        {
+            return null;
+        }
+
+        public void insert_before(LinkedListNode oldNode, LinkedListNode newNode)
+        {
+
+        }
+
+        public void insert_after(LinkedListNode oldNode, LinkedListNode newNode)
+        {
+
+        }
+
+        public void move_up(LinkedListNode node)
+        {
+
+        }
+
+        public void move_down(LinkedListNode node)
+        {
+
+        }
     }
 }

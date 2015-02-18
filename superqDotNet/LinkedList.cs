50595750a2fbb08b88e616f11499eefb89b1baaa
Daniel Manesajian <manesajian@gmail.com>
2015-02-17 23:04:27 -0800

    Implemented lookup().

--- superqDotNet/LinkedList.cs
+++ superqDotNet/LinkedList.cs
@@ -19,6 +19,44 @@ namespace superqDotNet
 
         }
 
+        private LinkedListNode lookup(int idx)
+        {
+            if (idx >= count)
+                throw new Exception("idx (" + idx.ToString() + ") out of range.");
+
+            if (count == 0)
+                return null;
+
+            LinkedListNode node = null;
+
+            if (idx >= 0)
+            {
+                // start from whichever end of list is closest to idx
+                int midIdx = (count - 1) / 2;
+                if (idx < midIdx)
+                {
+                    node = head;
+                    for (int i = 0; i < idx; ++i)
+                        node = node.next;
+                }
+                else
+                {
+                    node = tail;
+                    for (int i = 0; i < (count - 1) - idx; ++i)
+                        node = node.prev;
+                }
+            }
+            else
+            {
+                // handle negative idx
+                node = tail;
+                for (int i = 1; i < Math.Abs(idx); ++i)
+                    node = node.prev;
+            }
+
+            return node;
+        }
+
         public bool is_empty()
         {
             return count == 0;
@@ -26,49 +64,57 @@ namespace superqDotNet
 
         public void push(int idx, LinkedListNode node)
         {
-            if idx == 0:
+            if (idx == 0)
+            {
                 // set new head, order of operations matters
-                node.prev = None;
-                node.next = self.head;
+                node.prev = null;
+                node.next = head;
 
-            # if list not empty, point current head to new head
-            if self.head is not None:
-                self.head.prev = node
-            else:
-                self.tail = node
+                // if list not empty, point current head to new head
+                if (head != null)
+                    head.prev = node;
+                else
+                    tail = node;
 
-            self.head = node
-# TODO: this looks like an off by one. Should be count - 1. Test case?
-# Also, there is the question of >.
-        elif idx == self.__count:
-            # set new tail, order of operations matters
-            node.next = None
-            node.prev = self.tail
+                head = node;
+            }
+            else if (idx >= count - 1)
+            {
+                // set new tail, order of operations matters
+                node.next = null;
+                node.prev = tail;
 
-            # if list not empty, point current tail to new tail
-            if self.tail is not None:
-                self.tail.next = node
-            else:
-                self.head = node
+                // if list not empty, point current tail to new tail
+                if (tail != null)
+                    tail.next = node;
+                else
+                    head = node;
 
-            self.tail = node
-        else:
-            curNode = self.__lookup(idx)
+                tail = node;
+            }
+            else
+            {
+                LinkedListNode curNode = lookup(idx);
 
-            # handle empty list case
-            if curNode is None:
-                self.head = node
-                self.tail = node
-                node.prev = None
-                node.next = None
-            else:
-                # splice new node in
-                node.next = curNode
-                node.prev = curNode.prev
-                curNode.prev.next = node
-                curNode.prev = node
+                // handle empty list case
+                if (curNode == null)
+                {
+                    head = node;
+                    tail = node;
+                    node.prev = null;
+                    node.next = null;
+                }
+                else
+                {
+                    // splice new node in
+                    node.next = curNode;
+                    node.prev = curNode.prev;
+                    curNode.prev.next = node;
+                    curNode.prev = node;
+                }
+            }
 
-        self.__count += 1
+            count += 1;
         }
 
         public void push_head(LinkedListNode node)

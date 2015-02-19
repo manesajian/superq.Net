0e746eeeba314e4121c517be59dc22500e558ff5
Daniel Manesajian <manesajian@gmail.com>
2015-02-19 14:43:12 -0800

    Implemented FromString(). Stubbed in attach().

--- superqDotNet/superq.cs
+++ superqDotNet/superq.cs
@@ -8,16 +8,18 @@ namespace superqDotNet
 {
     public class superq
     {
-        public string name { get; set; }
-        public string host { get; set; }
-        public string publicName { get; set; }
-
-        public string keyCol { get; set; }
-        public int maxlen { get; set; }
-        public bool autoKey { get; set; }
-
         private LinkedList<superqelem> list = new LinkedList<superqelem>();
         private Dictionary<string, LinkedListNode> dict = new Dictionary<string, LinkedListNode>();
+
+        public string name;
+        public string host;
+        public string publicName;
+
+        public string keyCol;
+        public int maxlen;
+        public bool autoKey;
+
+        public bool attached = false;
 
         public superq(object initObj,
                       string name,
@@ -32,6 +34,33 @@ namespace superqDotNet
             keyCol = "";
             maxlen = 0;
             autoKey = false;
+
+            attached = false;
+        }
+
+        private object this[string propertyName]
+        {
+            get { return this.GetType().GetProperty(propertyName).GetValue(this, null); }
+            set { this.GetType().GetProperty(propertyName).SetValue(this, value, null); }
+        }
+
+        private void attach()
+        {
+            //if self.attached:
+            //    raise Exception('Already attached!')
+
+            //if self.dataStore.superq_exists(self.name):
+            //    raise NotImplemented('Not yet allowed to attach existing superqs.')
+
+            attached = true;
+
+            //self.dataStore.superq_create(self)
+
+            //# create each elem. The 1st one triggers backing table creation
+            //if len(self) > 0:
+            //    # add each superqelem now
+            //    for name, sqe in self.__internalDict.items():
+            //        self.create_elem_datastore_only(sqe);
         }
 
         public string ToString()
@@ -59,57 +88,63 @@ namespace superqDotNet
             return sqStr;
         }
 
-        public void FromString()
+        public void FromString(string sqStr, bool attach = false)
         {
-            //# initialize internal storage
-            //self.__internalList = LinkedList()
-            //self.__internalDict = {}
+            // initialize internal storage
+            list = new LinkedList<superqelem>();
+            dict = new Dictionary<string, LinkedListNode>();
 
-            //# separate out sq header from remainder
-            //headerSeparatorIdx = sqStr.index(';')
-            //sqHeader = sqStr[ : headerSeparatorIdx]
-            //sqStr = sqStr[headerSeparatorIdx + 1 : ]
+            // separate out sq header from remainder
+            int headerSeparatorIdx = sqStr.IndexOf(';');
+            string sqHeader = sqStr.Substring(0, headerSeparatorIdx);
+            sqStr = sqStr.Substring(headerSeparatorIdx + 1);
 
-            //# get name and number of fields from sq header
-            //headerElems = sqHeader.split(',')
-            //self.name = headerElems[0]
-            //numSqes = int(headerElems[1])
+            // get name and number of fields from sq header
+            string[] headerElems = sqHeader.Split(',');
+            name = headerElems[0];
+            int numSqes = Int32.Parse(headerElems[1]);
 
-            //# separate out attributes from remainder
-            //headerSeparatorIdx = sqStr.index(';')
-            //sqAttrs = sqStr[ : headerSeparatorIdx]
-            //sqStr = sqStr[headerSeparatorIdx + 1 : ]
+            // separate out attributes from remainder
+            headerSeparatorIdx = sqStr.IndexOf(';');
+            string sqAttrs = sqStr.Substring(0, headerSeparatorIdx);
+            sqStr = sqStr.Substring(headerSeparatorIdx + 1);
 
-            //# set attributes
-            //attrElems = sqAttrs.split(',')
-            //for attr in attrElems:
-            //    name, value = attr.split('|')
+            // set attributes
+            string[] attrElems = sqAttrs.Split(',');
+            foreach (string attr in attrElems)
+            {
+                string[] elems = attr.Split('|');
+                string attrName = elems[0];
+                string attrValue = elems[1];
 
-            //    if value.startswith('None'):
-            //        value = None
+                if (attrValue.StartsWith("None"))
+                    attrValue = null;
 
-            //    setattr(self, name, value)
+                this[attrName] = attrValue;
+            }
 
-            //if attach:
-            //    self.attach()
+            if (attach)
+                this.attach();
 
-            //# parse out each superqelem
-            //for i in range(0, numSqes):
-            //    # separate field length indicator from remainder
-            //    separatorIdx = sqStr.index(',')
-            //    elemLen = int(sqStr[ : separatorIdx])
-            //    sqStr = sqStr[separatorIdx + 1 : ]
+            // parse out each superqelem
+            for (int i = 0; i < numSqes; ++i)
+            {
+                // separate field length indicator from remainder
+                int separatorIdx = sqStr.IndexOf(',');
+                int elemLen = Int32.Parse(sqStr.Substring(0, separatorIdx));
+                sqStr = sqStr.Substring(separatorIdx + 1);
 
-            //    # slice the rest of the sqe out
-            //    sqeStr = sqStr[ : elemLen]
-            //    sqStr = sqStr[elemLen : ]
+                // slice the rest of the sqe out
+                string sqeStr = sqStr.Substring(0, elemLen);
+                sqStr = sqStr[elemLen : ];
 
-            //    # deserialize sqe from string fragment
-            //    sqe = superqelem(sqeStr, parentSq = self, buildFromStr = True)
+                // deserialize sqe from string fragment
+                superqelem sqe = new superqelem(sqeStr, null, this, true);
 
-            //    # add element to internal dictionary and tail of internal list
-            //    self.__internalDict[sqe.name] = sqe
-            //    self.__internalList.push_tail(sqe)
+                // add element to internal dictionary and tail of internal list
+                dict[sqe.name] = sqe;
+                list.push_tail(sqe);
+            }
         }
     }
 }

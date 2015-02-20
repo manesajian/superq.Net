3dadb2bf7cd3319b5bf25ae415d00b8164b1c84c
Daniel Manesajian <manesajian@gmail.com>
2015-02-19 19:39:00 -0800

    Implemented ToString() and IEnumerable interface.

--- superqDotNet/superqelem.cs
+++ superqDotNet/superqelem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,18 @@ namespace superqDotNet
 {
     public class superqelem : LinkedListNode
     {
+        // list of elematoms
+        private LinkedList<superqelem> list = new LinkedList<superqelem>();
+
+        // dictionary of elematoms, keyed by 'field' name
+        private Dictionary<string, LinkedListNode> dict = new Dictionary<string, LinkedListNode>();
+
         public string name;
         public object value;
         public superq parentSq;
+
+        public string valueType;
+
         public bool buildFromStr;
 
         public superqelem(string name,
@@ -21,9 +31,40 @@ namespace superqDotNet
 
         }
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<LinkedListNode> GetEnumerator()
+        {
+            LinkedListNode node = list.head;
+            while (node != null)
+            {
+                yield return node;
+                node = node.next;
+            }
+        }
+
+        private string toPyType(object variable)
+        {
+            return "str";
+        }
+
         public string ToString()
         {
-            return "";
+            string sqeStr = string.Format("{0},{1},{2},{3},{4};", toPyType(name),
+                                                                  name,
+                                                                  valueType,
+                                                                  value,
+                                                                  list.count);
+            foreach (elematom atom in this)
+            {
+                string elemStr = string.Format("{0}|{1}|{2};", atom.name, atom.type, atom.value);
+                sqeStr += string.Format("{0}|{1}", elemStr.Length, elemStr);
+            }
+
+            return sqeStr;
         }
 
         public void FromString(string sqe)

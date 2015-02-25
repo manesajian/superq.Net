3db32f42750264a303ea74f88bcd71e4c729a7da
Daniel Manesajian <manesajian@gmail.com>
2015-02-24 22:31:26 -0800

    Added first unit test.

---
+++ basicInteropTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using superqDotNet;
+
+namespace basicInteropTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BasicInstantiation()
+        {
+            superq sq = new superq(new int[0]);
+        }
+    }
+}

f782077c994962bb67952563febd6348c353d3a6
Daniel Manesajian <manesajian@gmail.com>
2015-03-16 23:12:43 -0700

    Added static connection manager class GlobalMgr.

---
+++ superqDotNet/GlobalMgr.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superqDotNet
+{
+    public static class GlobalMgr
+    {
+        private SuperQNetworkClientMgr _client = null;
+
+        public SuperQNetworkClientMgr client
+        {
+            get
+            {
+                return _client;
+            }
+            set
+            {
+                _client = value;
+            }
+        }
+    }
+}
